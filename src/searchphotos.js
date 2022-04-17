c92aa168984c9584363efa61d1b16203757afb7e
patrick mugambi <patrick@kali.pm.com>
2022-04-17 21:43:40 +0300

    create search form

--- src/searchphotos.js
+++ src/searchphotos.js
@@ -2,7 +2,18 @@ import React from "react";
 export default function SearchPhotos(){
     return (
         <>
-        
+        <form className="form">
+            <label className="label" htmlFor="query">
+                {" "}
+            </label>
+            <input type="text"
+            name="query" className="input"
+            placeholder = {`Try "nature" or "wildlife"`}
+            />
+            <button type="submit" className="button">
+                Search
+            </button>
+        </form>
         
         </>
     )
