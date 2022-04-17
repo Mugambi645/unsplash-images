43496666f49e6362a1243976ac27a6f868bbcf51
patrick mugambi <patrick@kali.pm.com>
2022-04-17 21:59:29 +0300

    create asynchronous function to search images on unsplash

--- src/searchphotos.js
+++ src/searchphotos.js
@@ -1,10 +1,24 @@
 import React, {useState} from "react";
-
+import Unsplash, {toJson} from "unsplash-js";
+const unsplash = new Unsplash({
+    accessKey: "",
+})
 export default function SearchPhotos(){
-    const [query, setQuery] = useState(" ")
+    const [query, setQuery] = useState(" ");
+    console.log(query);
+    const searchphotos = async(e) => {
+        e.preventDefault();
+        unsplash.search
+        .photos(query)
+        .then(toJson)
+        .then((json) => {
+            console.log(json);
+        })
+        console.log("Submitting the form");
+    }
     return (
         <>
-        <form className="form">
+        <form className="form" onSubmit={searchPhotos}>
             <label className="label" htmlFor="query">
                 {" "}
             </label>
